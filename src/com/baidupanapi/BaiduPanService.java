63e6f31fcfd49db3827ba3e0a95dac57351cf877
xm <402276694@qq.com>
2015-11-23 13:55:33 +0800

    文档更新

--- src/com/baidupanapi/BaiduPanService.java
+++ src/com/baidupanapi/BaiduPanService.java
@@ -14,8 +14,24 @@ public class BaiduPanService extends BaseClass{
         super(username, password, BaseData.apiTemplate, captchaRunnable);
     }
 
-
+    /**
+     * 获得配额信息
+     * @return 返回请求返回的字符串
+     *
+     * 返回正确时返回字符串中的数据结构
+     * {"errno":0,"total":配额字节数,"used":已使用字节数,"request_id":请求识别号}
+     * */
     public String quota(Map<String,Object> keyValueArgs) throws IOException {
         return request("quota",null,null,null,null,null,null,keyValueArgs);
+    }
+
+    /**
+     * 获取目录下的文件列表
+     *
+     *
+     * */
+    public String listFiles(String remotePath,String orderBy,String orderType,Integer startIndex,Integer endIndex, Map<String,Object> keyValueArgs){
+        //设置默认值
+
     }
 }
